2e8d555cee94f49fd1fee1d117816d08240cfd81
polapob <poom12341@hotmail.com>
2022-09-05 22:36:09 +0700

    finished homework

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -1,8 +1,43 @@
 class FormController < ApplicationController
   def index
-  end
+    @number_of_choices = params[:number_of_choices];
+    choices = @number_of_choices.to_i - 1;
+    @is_first_time = params[:is_first_time];
+    @subjectOld = [];
+    @scoreOld = [];
+    @sum_score = 0;
+    @max_score = 0;
+    @max_subject = "";
+    @isEmp = false;
 
+    if !@is_first_time
+      @is_blank = false
+      for i in 1..@number_of_choices.to_i+1
+        @name = params["subject_#{i}"]
+        @mark = params["mark_#{i}"]
+        if @name === "" || @mark === ""
+          @is_blank = true;
+        end
+
+        @prev_max_score = @max_score;
+        @max_score = [@mark.to_i,@max_score].max;
+
+        if @max_score != @prev_max_score
+          @max_subject = @name
+          end
+        @sum_score += @mark.to_i;
+      
+      end
+      if !@is_blank
+        redirect_to "/score/index?sumScore=#{@sum_score}&maxScoreSubject=#{@max_subject}"
+        end
+    end
+
+end
   def score
     
+  end
+  def home
+    @is_first_time = true;
   end
 end
--- app/controllers/score_controller.rb
+++ app/controllers/score_controller.rb
@@ -1,19 +1,6 @@
 class ScoreController < ApplicationController
   def index
-    @subject1 = params[:subject1]
-    @subject2 = params[:subject2]
-    @subject3 = params[:subject3]
-    @score1 = params[:mark1].to_i
-    @score2 = params[:mark2].to_i
-    @score3 = params[:mark3].to_i
-    @sumScore = @score1 + @score2 + @score3
-    @maxScore = [@score1,@score2,@score3].max
-    if @maxScore == @score1
-      @maxScoreSubject = @subject1;
-    elsif @maxScore == @score2
-      @maxScoreSubject = @subject2;
-    else
-      @maxScoreSubject = @subject3;
+    @sumScore = params[:sumScore]
+    @maxScoreSubject = params[:maxScoreSubject]
    end
-  end
 end
